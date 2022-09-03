741f637c412a30831f84aadb5f777d86bc246ca1
AndrewM <114.amart@gmail.com>
2022-09-03 15:47:30 +0100

    Initialisation
    Just adding in the initial files and datasets I've downloaded. Also included the gitignore file that is hiding the SRTM datasets to reduce size of repo.

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  3 15:31:55 2022
+
+@author: Andrew
+
+This will be the main file for generating the hexagons we desire from the topographical data.
+"""
+
+
+
