317aed80cc95d5504e39b8bfe56dd80799415f9a
AndrewM <114.amart@gmail.com>
2022-09-22 13:12:27 +0100

    Grid interpolation
    I've managed to get the interpolation from our image grid to the hexagonal grid working, and as the computations are paralelised its fairly quick and scalable.

--- HexGrid.py
+++ HexGrid.py
@@ -75,8 +75,86 @@ def layerAlgorithm(targetH,numHexInRadius=3):
     # adjust the origin so that the grid is centered on targetH
     vertices = vertices + targetH.centre
     return vertices, faces
-            
-            
+
+
+
+def interpolateGrids(v,X,Y,D,f=lambda x,y:-np.sqrt(x*x + y*y)+1):
+    '''
+    Function to interpolate the values D on an X-Y cartesian grid onto the HexGrid coordinates.
+    This will also allow for a weighting function, f (i.e. linear vs quadratic interpolation.)
+    
+    This does work on the assumption that X,Y are cartesian grids with unit spacing begining at the origin.
+    
+    INPUTS:
+        v: 2xa matrix of coordinate vectors for hexagon
+        X: nxm matrix, output of meshgrid for Cartesian coordinates
+        Y: nxm matrix, output of meshgrid for Cartesian coordinates
+        D: nxm matrix, data to be interpolated from Cartesian meshgrid to HexGrid
+        f: lambda function, ndarray->ndarray, needs to be able to take multiple elements and act elementwise/ be numpy compatible 
+    '''
+    nv = np.size(v,1) # number of vertices
+    
+    HexD = np.zeros((np.size(v,1))) # initialises the data matrix as 0
+    '''
+    print('Progress:',end='')
+    
+    for i in range(nv):
+        # for each vertex, calculate the appropriate value. Inefficient, but should work
+        x = v[0,i]
+        y = v[1,i]
+        
+        xu = np.floor(x)
+        xa = np.ceil(x)
+        
+        yu = np.floor(y)
+        ya = np.ceil(y)
+        
+        weights = np.array([ f(x-xu,y-yu) , f(x-xu,y-ya) , f(x-xa,y-yu) , f(x-xa,y-ya) ])
+        
+        Duu = D[np.logical_and(X == xu, Y == yu)]
+        Dua = D[np.logical_and(X == xu, Y == ya)]
+        Dau = D[np.logical_and(X == xa, Y == yu)]
+        Daa = D[np.logical_and(X == xa, Y == ya)]
+        
+        HexD[i] = (weights[0]*Duu + weights[1]*Dua + weights[2]*Dau + weights[3]*Daa ) / np.sum(weights)
+    
+        if not i % int(nv/100):
+            print('*',end='')
+    '''       
+    
+    # we can collapse X and Y to one dimension given they are Cartesian
+    x = np.unique(X,axis=0).reshape((np.size(X,1),))
+    y = np.unique(Y,axis=1).reshape((np.size(Y,0),))
+    
+    # will calculate the differences in an axm and axn matrix between vx and x; and vy and y respectively
+    xDiff = -np.full((nv,np.size(x)),x) + v[0,:].reshape((nv,1))
+    yDiff = -np.full((nv,np.size(y)),y) + v[1,:].reshape((nv,1))
+    
+    xUnder = (np.max(xDiff[xDiff < 0],axis=0) + v[0,:]).astype(int)
+    xAbove = (np.min(xDiff[xDiff > 0],axis=0) + v[0,:]).astype(int)
+    print('xUnder shape: {}'.format(xUnder.shape))
+    
+    yUnder = (np.max(yDiff[yDiff < 0],axis=0) + v[1,:]).astype(int)
+    yAbove = (np.min(yDiff[yDiff > 0],axis=0) + v[1,:]).astype(int)
+    print('yUnder shape: {}'.format(yUnder.shape))
+    
+    # we've now extracted the values of x and y in the grid adjacent to our hexgrid points
+    # we will calculate a weighted mean of the corners
+    D_xUyU = D[yUnder,xUnder]
+    print('D_xUyU shape: {}'.format(D_xUyU.shape))
+    D_xUyA = D[yAbove, xUnder]
+    D_xAyU = D[yUnder, xAbove]
+    D_xAyA = D[yAbove, xAbove]
+    
+    
+    weights = np.array([ f(v[1,:] - yUnder, v[0,:] - xUnder), f(v[1,:] - yAbove, v[0,:] - xUnder), f(v[1,:] - yUnder, v[0,:] - xAbove), f(v[1,:] - yAbove, v[0,:] - xAbove) ])
+    print('weights shape: {}'.format(weights.shape))
+    
+    HexD = ( weights[0,:]*D_xUyU + weights[1,:]*D_xUyA + weights[2,:]*D_xAyU + weights[3,:]*D_xAyA ) / np.sum(weights)
+    
+    return HexD
+
+
             
 '''
 o = 50
