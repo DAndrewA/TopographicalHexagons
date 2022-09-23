Fixed weighting function
I've adjusted the default weighting function in the grid interpolation, so that it can't go negative on the boundary x,y in [-1,1]. Before, this was possible at (1,1), etc, which was resulting in spiky terrain near elevation changes - most noticable at coasts.

Adding some constant to the given cos(x)cos(y) function will result in a smoothing function.

HexGrid.py
@@ -81,7 +81,9 @@ def layerAlgorithm(targetH,numHexInRadius=3):
 
 
 
-def interpolateGrids(v,x,y,D,f=lambda x,y:-np.sqrt(x*x + y*y)+1):
+# this weighting function is made so that x[y] = +-1 gives 0, and x[y] = 0 gives 1
+wFn = lambda x,y: np.cos(np.pi/2 * x)*np.cos(np.pi/2 * y) 
+def interpolateGrids(v, x, y, D, f=wFn):
     '''
     Function to interpolate the values D on an X-Y cartesian grid onto the HexGrid coordinates.
     This will also allow for a weighting function, f (i.e. linear vs quadratic interpolation.)
@@ -123,7 +125,7 @@ def interpolateGrids(v,x,y,D,f=lambda x,y:-np.sqrt(x*x + y*y)+1):
     weights = np.array([ f(v[1,:] - yUnder, v[0,:] - xUnder), f(v[1,:] - yAbove, v[0,:] - xUnder), f(v[1,:] - yUnder, v[0,:] - xAbove), f(v[1,:] - yAbove, v[0,:] - xAbove) ])
     print('weights shape: {}'.format(weights.shape))
     
-    HexD = ( weights[0,:]*D_xUyU + weights[1,:]*D_xUyA + weights[2,:]*D_xAyU + weights[3,:]*D_xAyA ) / np.sum(weights)
+    HexD = ( weights[0,:]*D_xUyU + weights[1,:]*D_xUyA + weights[2,:]*D_xAyU + weights[3,:]*D_xAyA ) / np.sum(weights,axis=0)
     
     return HexD
 

stl_creation.py
@@ -20,7 +20,7 @@ import HexGrid as HexGrid
 
 #------------------------------------------------
 
-seaVal = -10
+seaVal = -1000
 
 D1,m1 = LD.load_asc_format('data\\srtm_35_04\\','srtm_35_04.asc',seaVal)
 D2,m2 = LD.load_asc_format('data\\srtm_36_04\\','srtm_36_04.asc',seaVal)
@@ -29,7 +29,12 @@ D_combined = np.hstack((D1,D2))
 
 #------------------------------------------------
 
+del D1,D2 # save space
+
+lcd = 1
+newD = D_combined.copy()
 newD,lcd = LD.downsample_minimum(D_combined)
+
 newM = m1.copy()
 newM[2] = newM[2] * lcd
 Transform = CT.CoordinateTransform(metadata=newM)
@@ -38,8 +43,10 @@ Transform = CT.CoordinateTransform(metadata=newM)
 coords = np.array([-1.974412, 43.215345]).reshape((2,1)) # roughly the first point I want
 img = Transform.coords2Img(coords)
 
+HEXSIZE = int(1100/lcd)
+
 # create the hexagon centered on coords
-targetH = Hexagon.Hexagon(550,3,img)
+targetH = Hexagon.Hexagon(HEXSIZE,3,img)
 
 # create all the points in image space
 x = range(0,np.size(newD,1))
@@ -49,7 +56,7 @@ y = range(0,np.size(newD,0))
 
 
 ####
-NUMHEX = 550
+NUMHEX = 580
 
 v,f = HexGrid.layerAlgorithm(targetH,NUMHEX)
 
@@ -61,18 +68,26 @@ v,f = HexGrid.layerAlgorithm(targetH,NUMHEX)
 
 HexD = HexGrid.interpolateGrids(v,x,y,newD)
 
+newSeaVal = -10
+
+HexD[HexD < newSeaVal] = newSeaVal
+
 #ax = fig.add_subplot(111)
 #ax.contourf(v[0,:],v[1,:],HexD)
 
 #------------------------------------------------
-
-v,HexD,f = HexGrid.generateHexBase(NUMHEX, v, HexD, f, baseVal=-50)
+baseVal = -150
+v,HexD,f = HexGrid.generateHexBase(NUMHEX, v, HexD, f, baseVal)
 
 # SCALING of HexD
 
-HexD[HexD > seaVal] = HexD[HexD > seaVal] * 100000
 
-HexD[HexD == seaVal] = -20 
+
+
+scaleVal = 0.04
+
+HexD[HexD > newSeaVal] = HexD[HexD > newSeaVal] * scaleVal
+
 
 
 STL_tile = mesh.Mesh(np.zeros(f.shape[1], dtype=mesh.Mesh.dtype))
@@ -82,6 +97,13 @@ for i, face in enumerate(f.T):
         pos = int(pos)
         vec3 = [ *v[:,pos] , HexD[pos] ]
         STL_tile.vectors[i][j] = vec3
+ 
         
-STL_tile.save('data\\_stl\\TILE1.stl')
+# create a relevant STL filename so I can keep track of everything I've done
+tileNum = '1'
+folderName = 'data\\_stl\\' 
+fileName = 'TILE{}_n{}_b{:d}_c{:d}_s{}.stl'.format(tileNum,NUMHEX,-baseVal,-newSeaVal,scaleVal)
+
+STL_tile.save('data\\_stl\\' + fileName)
+print('File saved as {}'.format(fileName))
 print('Done :)')